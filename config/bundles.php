9aebc72f364432640c087f65aa96c13338a93141
olga-dev <olga.v.pivnenko@gmail.com>
2017-08-06 15:19:57 +0300

    composer req twig

--- config/bundles.php
+++ config/bundles.php
@@ -3,4 +3,5 @@
 return [
     'Symfony\Bundle\FrameworkBundle\FrameworkBundle' => ['all' => true],
     'Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle' => ['all' => true],
+    'Symfony\Bundle\TwigBundle\TwigBundle' => ['all' => true],
 ];
